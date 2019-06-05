0a518981f10103308f70f4f3491e79d6e0317efc
Jubert <jub8164@ymail.com>
2019-06-05 19:10:20 +0200

    Update fire.js

--- src/config/fire.js
+++ src/config/fire.js
@@ -4,13 +4,7 @@ import 'firebase/auth';
 
 //Initalize Firebase
 var config = {
-    apiKey: "AIzaSyATh6v4zNO34EVOiZbe-yPBjtVJCXeR3qA",
-    authDomain: "game-schedule-a6903.firebaseapp.com",
-    databaseURL: "https://game-schedule-a6903.firebaseio.com",
-    projectId: "game-schedule-a6903",
-    storageBucket: "game-schedule-a6903.appspot.com",
-    messagingSenderId: "843024978883",
-    appId: "1:843024978883:web:e11fdededd9213c7"
+   /*ur firebase congiguration*/
   };
   // Initialize Firebase
   firebase.initializeApp(config);
